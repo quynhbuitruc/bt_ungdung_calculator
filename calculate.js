5fdc07c2267a78450d587c596907fd0d21cb7774
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 23:35:46 +0900

    đặt number

--- calculate.js
+++ calculate.js
@@ -1,7 +1,7 @@
 function addition() {
-    let x = document.getElementById("so1").value;
+    let x = Number(document.getElementById("so1").value);
     console.log(x);
-    let y = document.getElementById("so2").value;
+    let y = Number(document.getElementById("so2").value);
     console.log(y);
     let tong = x + y;
     document.getElementById("hienthi").innerText = "Result addition :" + " " + tong;
