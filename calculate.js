9b1449923337085d29e91bfc402819b4a828144c
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 23:34:49 +0900

    chèn innertext

--- calculate.js
+++ calculate.js
@@ -3,7 +3,6 @@ function addition() {
     console.log(x);
     let y = document.getElementById("so2").value;
     console.log(y);
-    let tong = x + y
-
-    
+    let tong = x + y;
+    document.getElementById("hienthi").innerText = "Result addition :" + " " + tong;
 }
