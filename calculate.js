f8c7d5aeb77c7e59d5d2fedc735ba60434e868db
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 23:38:44 +0900

    tạp function cho 3 phép toán còn lại

--- calculate.js
+++ calculate.js
@@ -6,3 +6,30 @@ function addition() {
     let tong = x + y;
     document.getElementById("hienthi").innerText = "Result addition :" + " " + tong;
 }
+
+function subtraction() {
+    let x = Number(document.getElementById("so1").value);
+    console.log(x);
+    let y = Number(document.getElementById("so2").value);
+    console.log(y);
+    let hieu = x - y;
+    document.getElementById("hienthi").innerText = "Result subtraction :" + " " + hieu;
+}
+
+function multiplication() {
+    let x = Number(document.getElementById("so1").value);
+    console.log(x);
+    let y = Number(document.getElementById("so2").value);
+    console.log(y);
+    let tich = x * y;
+    document.getElementById("hienthi").innerText = "Result multiplication :" + " " + tich;
+}
+
+function division() {
+    let x = Number(document.getElementById("so1").value);
+    console.log(x);
+    let y = Number(document.getElementById("so2").value);
+    console.log(y);
+    let thuong = x / y;
+    document.getElementById("hienthi").innerText = "Result division :" + " " + thuong;
+}
