6e9deff73f785e934cfdfca096da92122aff3c61
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-14 22:26:36 +0900

    tạo click event pheptoan

--- upgraded.js
+++ upgraded.js
@@ -2,33 +2,59 @@
 document.getElementById("0").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value + "0";
 })
+
 document.getElementById("1").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "1"
 })
+
 document.getElementById("2").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "2"
 })
+
 document.getElementById("3").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "3"
 })
+
 document.getElementById("4").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "4"
 })
+
 document.getElementById("5").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "5"
 })
+
 document.getElementById("6").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "6"
 })
+
 document.getElementById("7").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "7"
 })
+
 document.getElementById("8").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "8"
 })
+
 document.getElementById("9").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "9"
 })
+
 document.getElementById(".").addEventListener("click", function () {
     document.getElementById("so1").value = document.getElementById("so1").value +  "."
+})
+
+document.getElementById("+").addEventListener("click", function() {
+    document.getElementById("pheptoan").value = "+"
+})
+
+document.getElementById("-").addEventListener("click", function() {
+    document.getElementById("pheptoan").value = "-"
+})
+
+document.getElementById("*").addEventListener("click", function() {
+    document.getElementById("pheptoan").value = "*"
+})
+
+document.getElementById("/").addEventListener("click", function() {
+    document.getElementById("pheptoan").value = "/"
 })
