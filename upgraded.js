0bf3b34f619a5cc2a9f5ab648019c77851938870
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-14 22:38:05 +0900

    sửa tên id

--- upgraded.js
+++ upgraded.js
@@ -1,60 +1,56 @@
 
 document.getElementById("0").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value + "0";
+    document.getElementById("temp").value = document.getElementById("temp").value + "0";
 })
 
 document.getElementById("1").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "1"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "1"
 })
 
 document.getElementById("2").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "2"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "2"
 })
 
 document.getElementById("3").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "3"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "3"
 })
 
 document.getElementById("4").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "4"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "4"
 })
 
 document.getElementById("5").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "5"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "5"
 })
 
 document.getElementById("6").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "6"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "6"
 })
 
 document.getElementById("7").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "7"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "7"
 })
 
 document.getElementById("8").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "8"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "8"
 })
 
 document.getElementById("9").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "9"
+    document.getElementById("temp").value = document.getElementById("temp").value +  "9"
 })
 
 document.getElementById(".").addEventListener("click", function () {
-    document.getElementById("so1").value = document.getElementById("so1").value +  "."
+    document.getElementById("temp").value = document.getElementById("temp").value +  "."
 })
 
-document.getElementById("+").addEventListener("click", function() {
-    document.getElementById("pheptinh").value = "+"
-})
+document.getElementById("+").addEventListener("click", function(cong)) 
 
-document.getElementById("-").addEventListener("click", function() {
-    document.getElementById("pheptinh").value = "-"
-})
+document.getElementById("-").addEventListener("click", function(tru))
 
-document.getElementById("*").addEventListener("click", function() {
-    document.getElementById("pheptinh").value = "*"
-})
+document.getElementById("*").addEventListener("click", function(nhan))
 
-document.getElementById("/").addEventListener("click", function() {
-    document.getElementById("pheptinh").value = "/"
-})
+document.getElementById("/").addEventListener("click", function(chia))
+
+function cong() {
+
+}
